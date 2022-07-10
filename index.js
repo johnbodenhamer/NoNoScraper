9c48fe584f81efa928487313f4199dd12ddb1565
johnbodenhamer <ryanbodenhamer@gmail.com>
2022-07-10 15:59:20 -0500

    WIP narrowing dataset

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = requrire('express');
+const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
@@ -8,12 +8,20 @@ app.get('/', (req, res) => {
 
     request(url, (error, response, html) => {
         if(!error) {
-            var $ = cheerio.load(html);
-            var innings = document.querySelectorAll('div.ScoreCell__Time');
+            const $ = cheerio.load(html);
+            const gameModule = $('.gameModules')
+            
+            // console.log(gameModule.text());
+            let list = [];
+            const cards = $('#fittPageContainer > div:nth-child(4) > div > div.PageLayout__Main > div:nth-child(1) > div > section > div').text();
+            // console.log(cards) 
+
+            const innings = $('[class*="ScoreCell__Time"]').text();
             console.log(innings);
         };
     });
 });
+
 
 app.listen('8080');
 console.log('NoNo is running on http://localhost:8080');
