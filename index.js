bff9990a6d19f5ae539cb960ef379c47490a7f00
johnbodenhamer <ryanbodenhamer@gmail.com>
2022-07-10 13:52:59 -0500

    get innings

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = requrire('express');
 const request = require('request');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 const app = express();
+
+app.get('/', (req, res) => {
+    let url = 'https://www.espn.com/mlb/scoreboard';
+
+    request(url, (error, response, html) => {
+        if(!error) {
+            var $ = cheerio.load(html);
+            var innings = document.querySelectorAll('div.ScoreCell__Time');
+            console.log(innings);
+        };
+    });
+});
+
+app.listen('8080');
+console.log('NoNo is running on http://localhost:8080');
+
+module.exports = app;
